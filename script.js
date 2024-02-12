1bd6dd424d064acefe92347b3b81521c339ba9a2
aman gupta <ag181373@gmail.com>
2024-02-12 07:37:38 +0530

    Add folder contents

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $(".animation").waypoint(function(){
+ $(".h1").addClass(
+     "animate__animated animate__slideInLeft"
+      );
+
+     $(".banner").addClass(
+         "animate__animated animate__slideInRight"
+            
+          );
+
+          $("#about-img").addClass(
+              "animate__animated animate__slideInLeft"
+              );
+
+             $("#About").addClass(
+            "animate__animated animate__slideInRight"
+        );
+
+        
+         $("#h1").addClass(
+            "animate__animated animate__slideInRight"
+            );
+
+            $("#paragraph").addClass(
+             "animate__animated animate__slideInRight"
+            );
+
+        $("#paragraph").addClass(
+         "animate__animated animate__slideInRight"
+         );
+
+            $("#skills").addClass(
+             "animate__animated animate__slideInRight"
+             );
+
+             $(". fa-html5,fa-css3-alt,fa-bootstrap").addClass(
+            "animate__animated animate__slideInRight"
+             );
+
+
+
+             $("#card1").addClass(
+                "animate__animated animate__slideInLeft"
+                 );
+    });
+
+
+
+});
